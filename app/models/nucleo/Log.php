e80fda3357e143106357b2780cc6439b3c0ce2c5
Joao Costa <joao.costa@unimestre.com>
2022-04-21 20:18:16 -0300

    Feat: Implementada classe para gravar log de operações do sistema. Por enquanto, apenas envia log por email.

---
+++ app/models/nucleo/Log.php
@@ -0,0 +1,51 @@
+<?php
+namespace app\models\nucleo;
+
+require_once '../../../vendor/autoload.php';
+require_once('../../../config.php');
+use app\models\nucleo\Email;
+
+class Log
+{
+    private $Email;
+
+    public function __construct() {
+        $this->Email = new Email();
+    }
+
+    public function gravarLog($conteudo) {
+        $this->Email->enviarEmailLog($conteudo);
+    }
+
+    public function geraLogCamposInclusao($novos) {
+        $log = '';
+
+        foreach ($novos as $key => $value) {
+        $log.= $key.': '.$novos[$key].PHP_EOL;
+        }
+
+        return $log;
+    }
+
+    public function geraLogCamposAlteracao($antigos, $novos, $nivel = 1) {
+        $log = '';
+
+        if( $novos == null || $antigos == null   ) {
+        return $log ;
+        }
+
+        foreach ($antigos as $key => $value) {
+        if($novos[$key] instanceof stdClass){
+            $novos[$key] = (array) $novos[$key];
+        }
+
+        if( $value != null && is_array($value) && $novos[$key] != null && !empty($novos[$key])){
+            $log.= $this->geraLogCamposAlteracao($antigos[$key], $novos[$key], $nivel + 1 );
+        }else if (array_key_exists ( $key, $novos ) && $antigos[$key] != $novos[$key]) {
+            $log.= $key.': '.$antigos[$key] .' para '. $novos[$key].PHP_EOL;
+        }
+        }
+
+        return $log;
+    }
+}
