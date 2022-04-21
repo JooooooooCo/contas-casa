042c3eb18d2cfaf2e1de403c72be7db7cb76b8ef
Joao Costa <joao.costa@unimestre.com>
2022-04-21 20:16:29 -0300

    Feat: Adicionadas configurações padrões para envio de email, no arquivo config_start.php

--- config_start.php
+++ config_start.php
@@ -1,7 +1,17 @@
 <?php
+// Definir as configurações do sistema e renomear este arquivo para "config.php"
+
 // Define as configurações de conexão com o banco
-// Inserir dados de conexão e renomear este arquivo para "config.php"
 define("BANCO", 'mysql:host=XXXXXX;port=XXXXXX;dbname=XXXXXX');
 define("BANCO_USUARIO", 'XXXXXX');
 define("BANCO_SENHA", 'XXXXXX');
+
+// Define as configurações de email, para envio de notificações
+define("EMAIL_LOG_ATIVO", false);
+define("EMAIL_LOG_PORT", 587);
+define("EMAIL_LOG_HOST", 'smtp.mail.yahoo.com');
+define("EMAIL_LOG_USER", 'examplo@yahoo.com');
+define("EMAIL_LOG_SENHA", 'senha');
+define("EMAIL_LOG_PARA", 'examplo@gmail.com');
+define("EMAIL_LOG_DE", 'examplo@yahoo.com');
 ?>
