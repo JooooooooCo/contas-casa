36e6b5aacbd6f9e48f97a982d87184be6cde1b74
Joao Costa <joao.costa@unimestre.com>
2022-04-25 01:47:39 -0300

    Feat: Melhorias no mixinAlert

--- public/js/nucleo/mixin-alert.js
+++ public/js/nucleo/mixin-alert.js
@@ -25,22 +25,36 @@ var mixinAlert = {
             Swal.close();
         },
 
-        async mixinAlertProsseguir(ds_texto_prosseguir = 'Deseja realmente prosseguir?', sn_botao_opcional = false, ds_texto_botao_opcional = '') {
+        async mixinAlertProsseguir(
+            ds_texto_prosseguir = 'Deseja realmente prosseguir?',
+            sn_botao_opcional = false,
+            ds_texto_botao_opcional = 'Não',
+            sn_botao_confirmar = true,
+            ds_texto_botao_confirmar = 'OK',
+            sn_botao_cancelar = true,
+            ds_texto_botao_cancelar = 'Cancelar',
+        ) {
             result = await Swal.fire({
                 icon: 'warning',
                 title: 'Atenção',
                 html: ds_texto_prosseguir,
-                confirmButtonText: 'OK',
-                showCancelButton: true,
-                cancelButtonText: 'Cancelar',
+                showConfirmButton: sn_botao_confirmar,
+                confirmButtonText: ds_texto_botao_confirmar,
+                showCancelButton: sn_botao_cancelar,
+                cancelButtonText: ds_texto_botao_cancelar,
                 showDenyButton: sn_botao_opcional,
                 denyButtonText: ds_texto_botao_opcional,
+                allowOutsideClick: false,
+                allowEscapeKey: false,
+                allowEnterKey: false
             });
 
             return result.value;
         },
 
         async mixinAlertErro(ds_texto_erro = 'Houve um erro durante a operação.') {
+            this.mixinAlertCarregando(false);
+
             await Swal.fire({
                 icon: 'error',
                 title: 'Oops...',
@@ -49,6 +63,8 @@ var mixinAlert = {
         },
 
         async mixinAlertSucesso(ds_texto_erro = 'Operação concluída com sucesso.') {
+            this.mixinAlertCarregando(false);
+
             await Swal.fire({
                 icon: 'success',
                 title: 'Sucesso',
