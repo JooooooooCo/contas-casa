dcbdd8682f16132c29e0961ae5a90719bf3827a7
JooooooooCo <costa.joaomf@gmail.com>
2025-01-06 23:37:56 -0300

    fix: log files generation

--- app/models/nucleo/Log.php
+++ app/models/nucleo/Log.php
@@ -14,23 +14,32 @@ class Log
     }
 
     public function gravarLog($conteudo) {
+        error_reporting(E_ERROR);
         $conteudo .= 'Data da operacao: ' . date("Y-m-d H:i:s") . PHP_EOL;
         $conteudo .= '----------------------------------------------------------------' . PHP_EOL;
-        file_put_contents('../../../log.log', $conteudo, FILE_APPEND);
-        file_put_contents('../../../log_temporario.log', $conteudo, FILE_APPEND);
+
+        $arquivos = ['../../../log.log', '../../../log_temporario.log'];
+
+        foreach ($arquivos as $arquivo) {
+            if (!file_exists($arquivo)) {
+                touch($arquivo);
+            }
+            file_put_contents($arquivo, $conteudo, FILE_APPEND);
+        }
     }
 
     public function enviarEmailLog() {
+        error_reporting(E_ERROR);
         $ds_arquivo_log = "../../../log_temporario.log";
 
         if (!file_exists($ds_arquivo_log)) {
             throw new \Exception('Erro ao abrir o arquivo de log temporário');
         }
 
-        $objArquivoLog = fopen( $ds_arquivo_log, "r" );
-        $nr_tamanho_arquivo = filesize( $ds_arquivo_log );
-        $ds_conteudo_log = fread( $objArquivoLog, $nr_tamanho_arquivo );
-        fclose( $objArquivoLog );
+        $objArquivoLog = fopen($ds_arquivo_log, "r");
+        $nr_tamanho_arquivo = filesize($ds_arquivo_log);
+        $ds_conteudo_log = fread($objArquivoLog, $nr_tamanho_arquivo);
+        fclose($objArquivoLog);
 
         $ds_conteudo_log = "<pre>$ds_conteudo_log</pre>";
 
@@ -42,7 +51,7 @@ class Log
         $log = '';
 
         foreach ($novos as $key => $value) {
-            $log.= $key.': '.$novos[$key].PHP_EOL;
+            $log .= $key . ': ' . $novos[$key] . PHP_EOL;
         }
 
         return $log;
@@ -51,19 +60,19 @@ class Log
     public function geraLogCamposAlteracao($antigos, $novos, $nivel = 1) {
         $log = '';
 
-        if( $novos == null || $antigos == null   ) {
-            return $log ;
+        if ($novos == null || $antigos == null) {
+            return $log;
         }
 
         foreach ($antigos as $key => $value) {
-            if($novos[$key] instanceof stdClass){
+            if ($novos[$key] instanceof \stdClass) {
                 $novos[$key] = (array) $novos[$key];
             }
 
-            if( $value != null && is_array($value) && $novos[$key] != null && !empty($novos[$key])){
-                $log.= $this->geraLogCamposAlteracao($antigos[$key], $novos[$key], $nivel + 1 );
-            } else if (array_key_exists ( $key, $novos ) && $antigos[$key] != $novos[$key]) {
-                $log.= $key.': '.$antigos[$key] .' para '. $novos[$key].PHP_EOL;
+            if ($value != null && is_array($value) && $novos[$key] != null && !empty($novos[$key])) {
+                $log .= $this->geraLogCamposAlteracao($antigos[$key], $novos[$key], $nivel + 1);
+            } else if (array_key_exists($key, $novos) && $antigos[$key] != $novos[$key]) {
+                $log .= $key . ': ' . $antigos[$key] . ' para ' . $novos[$key] . PHP_EOL;
             }
         }
 
